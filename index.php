9aa6fd7da9ac08acada28755389d15e663610cec
trish <166978173+tshamaec@users.noreply.github.com>
2024-05-16 21:26:26 -0930

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+//Task1
+$name = 'Trisha Mae Catibog';
+$age = 22;
+$address = 'Valenzuela';
+$hobbies = ('Listening to music, Shopping, Travel');
+$pet_peeve = 'Falsifier';
+
+echo $name, '<br>';
+echo $age, '<br>';
+echo $address, '<br>';
+echo $hobbies, '<br>';
+echo $pet_peeve, '<br><br>';
+
+//Task2
+$name = 'Trisha Mae Catibog';
+$age = 22;
+$address = 'Valenzuela';
+// $hobbies = ('Listening to music, Shopping, Travel');
+// $pet_peeve = 'Falsifier';
+
+
+//Task3
+define('NAME', 'Trisha Mae Catibog');
+define('AGE', 22);
+define('ADDRESS', 'Valenzuela');
+define('HOBBIES', 'Listening to music, Shopping, Travel');
+define('PET_PEEVE', 'Falsifier');
+
+echo NAME, '<br>';
+echo AGE, '<br>';
+echo ADDRESS, '<br>';
+echo HOBBIES, '<br>';
+echo PET_PEEVE, '<br>';
+
+
+//Task4
+echo '<pre>';
+var_dump (NAME, AGE, ADDRESS, HOBBIES, PET_PEEVE);
+echo '</pre>';
+
+
+//Task5
+/* 
+The explanation for why I dislike liars, particularly, is that when they lie to you, you will come to mistrust them.
+*/
